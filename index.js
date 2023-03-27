0561ed8c93b1693609651453f179a98de24d5449
ted <ted@warpedpuppy.com>
2023-03-27 19:09:11 -0400

    simplify post endpoint

--- index.js
+++ index.js
@@ -159,49 +159,39 @@ app.get(
 //Allow new users to register
 app.post(
   "/users",
-  [
-    check("Username", "Username is required").isLength({ min: 5 }),
-    check(
-      "Username",
-      "Username contains non-alphanumeric characters - not allowed."
-    ).isAlphanumeric(),
-    check("Password", "Password is required, min 6 characters").isLength({
-      min: 6,
-    }),
-    check("Email", "Email does not appear to be valid").isEmail(),
-  ],
   (req, res) => {
-    let errors = validationResult(req);
+	res.json({success: "success"})
+    // let errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.array() });
-    }
+    // if (!errors.isEmpty()) {
+    //   return res.status(422).json({ errors: errors.array() });
+    // }
 
-    let hashedPassword = Users.hashPassword(req.body.Password);
-    Users.findOne({ Username: req.body.Username })
-      .then((user) => {
-        if (user) {
-          return res.status(400).send(req.body.Username + "already exists");
-        } else {
-          Users.create({
-            Username: req.body.Username,
-            Password: hashedPassword,
-            Email: req.body.Email,
-            Birthday: req.body.Birthday,
-          })
-            .then((user) => {
-              res.status(201).json(user);
-            })
-            .catch((error) => {
-              console.error(error);
-              res.status(500).send("Error: " + error);
-            });
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-        res.status(500).send("Error: " + error);
-      });
+    // let hashedPassword = Users.hashPassword(req.body.Password);
+    // Users.findOne({ Username: req.body.Username })
+    //   .then((user) => {
+    //     if (user) {
+    //       return res.status(400).send(req.body.Username + "already exists");
+    //     } else {
+    //       Users.create({
+    //         Username: req.body.Username,
+    //         Password: hashedPassword,
+    //         Email: req.body.Email,
+    //         Birthday: req.body.Birthday,
+    //       })
+    //         .then((user) => {
+    //           res.status(201).json(user);
+    //         })
+    //         .catch((error) => {
+    //           console.error(error);
+    //           res.status(500).send("Error: " + error);
+    //         });
+    //     }
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //     res.status(500).send("Error: " + error);
+    //   });
   }
 );
 
